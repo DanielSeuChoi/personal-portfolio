1a184c9bb0fd128588c4ebfa5d71a8d3c6deecb5
DanielSeuChoi <choidanosa@gmail.com>
2024-04-26 16:57:16 -0700

    updated image size

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ import Button from './ui/Button';
 
 type ProjectProps = (typeof projectsData)[number];
 
-const Projects = ({ imageUrl, description, title, hash,tags }: ProjectProps) => {
+const Projects = ({ imageUrl, description, title, hash, tags }: ProjectProps) => {
     return (
         <div className='relative justify-center flex flex-col p-10'>
             <div className='mt-5'>
@@ -18,33 +18,33 @@ const Projects = ({ imageUrl, description, title, hash,tags }: ProjectProps) => 
                 <p className='leading-relaxed'>
                     {description}
                 </p>
-                <br/>
+                <br />
                 <ul className="flex flex-wrap  mb-4 gap-2 sm:mt-auto">
-                        {tags.map((tag, index) => (
-                            <li
-                                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                                key={index}
-                            >
-                                {tag}
-                            </li>
-                        ))}
-                    </ul>
+                    {tags.map((tag, index) => (
+                        <li
+                            className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                            key={index}
+                        >
+                            {tag}
+                        </li>
+                    ))}
+                </ul>
                 <div className="hover:bg-white" >
                     <Image
-                        className='relative w-[28.25rem] rounded-t-lg shadow-2xl transition hover:bg-white'
+                        className='relative h-[10.25rem] w-[20.25rem] rounded-t-lg shadow-2xl transition hover:bg-white'
                         src={imageUrl}
                         alt={'Photo'}
                     />
-                    </div>
-                </div>
-                <div className='flex flex-row justify-start mt-5'>
-                    <Button variant={'outline'} >
-                        <Link className='text-[1.3rem] flex flex-row text-left' href={hash}>
-                            {title}
-                        </Link>
-                    </Button>
                 </div>
             </div>
+            <div className='flex hover:opacity-70 flex-row justify-start mt-5'>
+                <Button variant={'outline'} >
+                    <Link className='text-[1.3rem] flex flex-row text-left' href={hash}>
+                        {title}
+                    </Link>
+                </Button>
+            </div>
+        </div>
     )
 }
 
